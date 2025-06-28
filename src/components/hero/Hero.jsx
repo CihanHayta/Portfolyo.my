1889e46bd27f85fdb73639e4b2a4e9fcb50eeefd
CihanHayta <cihanhayta8080@gmail.com>
2025-06-28 17:19:31 +0300

    üpdate

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
       <div className={styles.bottomBlur} />
 
       <img
-        src={"src/assets/hero/WhatsApp Image 2025-05-01 at 20.37.41 kopyası.jpeg"}
+        src={"src/assets/hero/WhatsApp Image 2025-05-01 at 20.37.41 kopyasi.png"}
         alt="Hero image of me"
         className={styles.heroImg}
       />
