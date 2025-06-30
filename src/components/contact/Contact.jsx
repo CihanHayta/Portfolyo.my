140ec1591df4546765e29ac74f2459420774ad32
CihanHayta <cihanhayta8080@gmail.com>
2025-07-01 00:14:35 +0300

    eftl

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,22 +6,22 @@ const Contact = () => {
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
         <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <p className={styles.subtitle}>Feel free to reach out!</p>
       </div>
 
       <ul className={styles.links}>
         <li className={styles.link}>
-         <img src="public/assets/contact/emailIcon.png" alt="Email Icon" />
+         <img src="/assets/contact/emailIcon.png" alt="Email Icon" />
           <a href="mailto:cihanhayta8080@gmail.com">cihanhayta8080@gmail.com</a>
         </li>
 
         <li className={styles.link}>
-          <img src="public/assets/contact/linkedinIcon.png" alt="LinkedIn Icon" />
+          <img src="/assets/contact/linkedinIcon.png" alt="LinkedIn Icon" />
           <a href="https://www.linkedin.com/in/cihan-hayta-4586671a0?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank">linkedin.com/CihanHayta</a>
         </li>
 
         <li className={styles.link}>
-         <img src="public/assets/contact/githubIcon.png" alt="GitHub Icon" />
+         <img src="/assets/contact/githubIcon.png" alt="GitHub Icon" />
           <a href="https://github.com/CihanHayta" target="_blank">github.com/CihanHayta</a>
         </li>
       </ul>
